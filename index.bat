0c6770338e7f1804931f5cd7e88dfb5aeb7f4c3f
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 13:48:42 +0100

    Push Update

---
+++ index.bat
@@ -0,0 +1,1 @@
+gcc "D:/K/Coding/projects/Cpp-Basic_GUI/index.cpp" -o "D:/K/Coding/projects/Cpp-Basic_GUI/index"&&"D:/K/Coding/projects/Cpp-Basic_GUI/index"&&cd "D:/K/Coding/projects/Cpp-Basic_GUI/"&&del index.exe
